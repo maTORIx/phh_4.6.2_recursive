a59b76fa5a379156382c253ad541b934bfcc7469
tori <maTORIxmatrix@gmail.com>
2017-06-28 17:12:22 +0900

    Update recursive.js

--- recursive.js
+++ recursive.js
@@ -5,7 +5,7 @@ function factorial(n) {
 
 function factorial2(n){
     var result = 1;
-    for(var i = 1; i <= n ;i++) {
+    for(var i = 2; i <= n ;i++) {
         result = result * i;
     }
     return result;
